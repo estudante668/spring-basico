975b79941c4cbd72c7bac5a490ac6adde534c20e
Fabricio Netto <desenhista668@gmail.com>
2025-08-18 10:48:52 -0300

    ESTUDO SPRING

---
+++ app-loja-bakend/backend/src/main/java/com/loja/backend/categoria/Categoria.java
@@ -0,0 +1,5 @@
+package com.loja.backend.categoria;
+
+public class Categoria {
+
+}
---
+++ app-loja-bakend/backend/src/main/java/com/loja/backend/modelo/Modelo.java
@@ -0,0 +1,46 @@
+package com.loja.backend.modelo;
+
+import java.math.BigDecimal;
+import java.util.Locale.Category;
+
+import com.loja.backend.categoria.Categoria;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data                   // Lombok cria os acessos da classe
+@NoArgsConstructor      // Construtor vazio para instanciamentos
+@AllArgsConstructor     // Construtor com entras
+@Builder                // Permite usar o padrão Builder
+public class Modelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotNull    
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    private String description;
+
+    private boolean active;
+
+}
+
